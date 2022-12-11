b25d3e5a53517834abbd30c1461a22180b8629d7
Николай Лукьянчиков <nicoolas.ru@gmail.com>
2022-12-11 18:01:40 +0300

    Шаг 1 и шаг 2 готовы

---
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -0,0 +1,11 @@
+package ru.hogwarts.school.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hogwarts.school.model.Avatar;
+
+public interface AvatarRepository extends JpaRepository<Avatar, Long> {
+
+    //Faculty findDistinctFacultyByStudents_Id(Long id);
+    Avatar findDistinctAvatarByStudent_Id(Long id);
+
+}
